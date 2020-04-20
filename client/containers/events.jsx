e18ff6a35d2218a7a3e6645f8aa03c6cae924a66
Brianna Sookhoo <briannasookhoo@Briannas-MacBook-Air.local>
2020-04-20 15:43:10 -0700

    delete event functionality written

--- client/containers/events.jsx
+++ client/containers/events.jsx
@@ -15,6 +15,7 @@ class Events extends Component {
       events: [],
     };
   }
+
   componentDidMount() {
     axios.get('/events').then((response) => {
       // console.log(response.data);
@@ -23,6 +24,14 @@ class Events extends Component {
       console.log(this.state);
     });
   }
+
+  deleteEvent(id) {
+    axios.delete('/events'+id)
+    this.setState({
+      events: this.state.events.filter(el => el.id !== id)
+    })
+  }
+
   render() {
     return (
       <Grid container spacing={2}>
--- server/controllers/eventsControllers.js
+++ server/controllers/eventsControllers.js
@@ -78,11 +78,6 @@ eventsControllers.createPost = async (req, res, next) => {
 eventsControllers.updatePost = async (req, res, next) => {
   const { name, location, date, description, user_id } = req.body; // req.body comes from client and when user submits, fetch arrives from client with data
   const { id } = req.params;
-  // const name = 'Brian';
-  // const location = 'Brians house';
-  // const date = '2020-04-18';
-  // const description = 'party';
-  // const user_id = 1;
 
   try {
     const text = 'UPDATE events SET name = $1, location = $2, date = $3, description = $4, user_id = $5 WHERE id = $6';
