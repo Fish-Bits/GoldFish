01b6cbf58e805f9f96b30212b3857d58cfc04809
Alon <61472253+alon25@users.noreply.github.com>
2020-04-20 09:40:40 -0700

    UI

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
