fe959b3e62efe117f8d940808128c687f83618f6
Alon <61472253+alon25@users.noreply.github.com>
2020-04-20 16:33:14 -0700

    Merging

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -18,10 +18,6 @@ const Header = (props) => {
     <div>
      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <a className="navbar-brand" href="#">FishBits</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
           <ul className="navbar-nav">
             {/* <li className="nav-item active">
               <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
@@ -30,7 +26,6 @@ const Header = (props) => {
               <Link to="/create" className="nav-link">New Event</Link>
             </li>
           </ul>
-        </div>
       </nav>
       <WaveBorder
         upperColor="#333a3f"
--- client/components/image.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Fish from '../assets/iStock-504085304.jpg';
-
-const Image = (props) => {
-  return <div styles={{backgroundImage: {Fish}}} alt="Fish" />
-}
-
-export default Image;
