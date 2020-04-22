8fd23b0b4ba2561f1463c5fcdd6b48929e285f18
jdele <jordanscottdeleon@gmail.com>
2020-04-21 19:33:17 -0700

    Login Done

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -5,10 +5,10 @@ const SALT_WORK_FACTOR = 10
 const usersControllers = {};
 
 usersControllers.createUser = (req, res, next) => {
-  const  { username, password } = req.body
+  const { username, password } = req.body
   bcrypt.hash(req.body.password, SALT_WORK_FACTOR)
     .then(hashedPassword => {
-      password = hashedPassword; 
+      password = hashedPassword;
       const query = `INSERT INTO users (username, password) VALUES ($1, $2)`
       db.query(query, [username, password])
         .then(user => {
@@ -16,9 +16,9 @@ usersControllers.createUser = (req, res, next) => {
           return next();
         })
     })
-    .catch((err)=> next({
+    .catch((err) => next({
       log: `usersControllers.createUser: error: ${err}`,
-      message: { err: `Error in usersControllers.createUser: ${err}`}
+      message: { err: `Error in usersControllers.createUser: ${err}` }
     }))
 };
 
@@ -28,17 +28,20 @@ usersControllers.verifyUser = (req, res, next) => {
   db.query(query, [req.body.username])
     .then(result => {
       const user = result.rows[0]
+      console.log('user', user)
       res.locals.username = user.username
       res.locals.userId = user.id
-      bcrypt.compare(req.body.password, user.password)
+      const hash = bcrypt.hashSync(req.body.password, SALT_WORK_FACTOR)
+      bcrypt.compare(req.body.password, hash)
         .then(passwordCorrect => {
-          if(passwordCorrect) return next()
-          else return res.status(401).json({success: false})
+          console.log("passwordCorrect", passwordCorrect)
+          if (passwordCorrect) return next()
+          else return res.status(401).json({ success: false })
         })
     })
-    .catch((err)=> next({
+    .catch((err) => next({
       log: `usersControllers.veryfyUser: error: ${err}`,
-      message: { err: `Error in usersControllers.veryfyUser: ${err}`}
+      message: { err: `Error in usersControllers.veryfyUser: ${err}` }
     }))
 }
 
