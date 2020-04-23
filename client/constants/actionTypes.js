5b13c2fca5e891b085bda034ae65a537505c091c
michael coker <mikey@michaels-MacBook-Pro.local>
2020-04-23 16:52:21 -0600

    created the post commment functionality

--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,4 +1,4 @@
 export const GET_USER = 'GET_USER';
 export const LOGIN = 'LOGIN';
-
-export const GET_COMMENTS = 'GET_COMMENTS'
+export const GET_COMMENTS = 'GET_COMMENTS';
+export const ADD_COMMENTS = 'ADD_COMMENTS';
