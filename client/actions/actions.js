3c399bba6e961b91cff3fbb0c9a2d70743deeeec
michael coker <mikey@michaels-MacBook-Pro.local>
2020-04-22 18:33:02 -0600

    added login action and refactored login component

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -3,7 +3,35 @@ import * as types from '../constants/actionTypes';
 
 const fakeUser = {
   username: 'augustine',
-  id: 0
+  id: 1
+}
+
+export const login = (user) => {
+  const {username, password} = user
+  return (dispatch) => {
+      fetch('/auth/login', {
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        localStorage.setItem("token", result.token)
+        dispatch({
+          type: types.LOGIN,
+          payload: result.username
+        })
+        if (result && result.success) {
+          console.log('success');
+        }
+      });
+  }
 }
 
 export const getUser = () => {
@@ -16,3 +44,29 @@ export const getUser = () => {
     })
   }
 }
+
+
+// const Login = (props) => {
+//   const classes = useStyles();
+//   const doLogin = () => {
+//     if (!values.username) return;
+//     if (!values.password) return;
+//     fetch('/auth/login', {
+//       method: 'post',
+//       headers: {
+//         Accept: 'application/json',
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         username: values.username,
+//         password: values.password,
+//       }),
+//     })
+//       .then((response) => response.json())
+//       .then((result) => {
+//         console.log(result)
+//         if (result && result.success) {
+//           console.log('success');
+//         }
+//       });
+//   };
